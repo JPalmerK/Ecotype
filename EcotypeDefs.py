17218782a38cc83ae4d9ec9ccd111b6db9cb2ec2
JPalmerK <kpalmer@coa.edu>
2024-07-11 07:30:22 -0700

    add vars to hdf5
    add UTC to hdf5 output, add shuffle option to batch loader (for testing), comment unused versions of batch loaders. Add activation options for models. Create wider models, fix confusion matrix outputs

--- EcotypeDefs.py
+++ EcotypeDefs.py
@@ -128,6 +128,7 @@ def create_hdf5_dataset(annotations, hdf5_filename):
             provider = row['Provider']
             kw = row['KW']
             kwCertin = row['KW_certain']
+            utc = row['UTC']
 
             spec, SNR = load_and_process_audio_segment(file_path, start_time, end_time)
 
@@ -143,6 +144,7 @@ def create_hdf5_dataset(annotations, hdf5_filename):
             dataset.create_dataset(f'{ii}/KW', data=kw)
             dataset.create_dataset(f'{ii}/KW_certain', data=kwCertin)
             dataset.create_dataset(f'{ii}/SNR', data=SNR)
+            dataset.create_dataset(f'{ii}/UTC', data=utc)
             
             
             print(ii, ' of ', len(annotations))
@@ -168,6 +170,7 @@ def process_row(row):
     provider = row['Provider']
     kw = row['KW']
     kwCertin = row['KW_certain']
+    
 
     spec, SNR = load_and_process_audio_segment(file_path, start_time, end_time)
 
@@ -233,6 +236,7 @@ class BatchLoader2(keras.utils.Sequence):
         # If a frequency limit is set then  figure out what that is now
         self.minFreq = minFreq
         self.minIdx = 0
+        
         #This is for trimming the frequency range
         if math.isfinite(self.minFreq):
              mel_frequencies = librosa.core.mel_frequencies(n_mels= self.specSize[0]+2)
@@ -269,11 +273,97 @@ class BatchLoader2(keras.utils.Sequence):
             return batch_data, batch_labels
         else:
             return np.array(batch_data), keras.utils.to_categorical(np.array(batch_labels), num_classes=self.n_classes)
+    def __shuffle__(self):
+        np.random.shuffle(self.indexes)
+        print('shuffled!')
+    
     
     def on_epoch_end(self):
         if self.shuffle:
             np.random.shuffle(self.indexes)
             print('Epoc end all shuffled!')
+
+
+import h5py
+import numpy as np
+import keras.utils
+import math
+import librosa
+
+# class BatchLoader2pt5(keras.utils.Sequence):
+#     def __init__(self, hdf5_file, batch_size=250, trainTest='train',
+#                  shuffle=True, n_classes=7, return_data_labels=False,
+#                  minFreq='nan', return_vars=None):
+#         self.hf = h5py.File(hdf5_file, 'r')
+#         self.batch_size = batch_size
+#         self.trainTest = trainTest
+#         self.shuffle = shuffle
+#         self.n_classes = n_classes
+#         self.return_data_labels = return_data_labels
+#         self.return_vars = return_vars if return_vars else []
+#         self.data_keys = list(self.hf[trainTest].keys())
+#         self.num_samples = len(self.data_keys)
+#         self.indexes = np.arange(self.num_samples)
+
+#         # Get the spectrogram size, assuming data exists in 'train'
+#         self.train_group = self.hf[trainTest]
+#         self.first_key = list(self.train_group.keys())[0]
+#         self.data = self.train_group[self.first_key]['spectrogram']
+#         self.specSize = self.data.shape
+        
+#         # If a frequency limit is set, figure out what that is now
+#         self.minFreq = minFreq
+#         self.minIdx = 0
+#         if math.isfinite(self.minFreq):
+#             mel_frequencies = librosa.core.mel_frequencies(n_mels=self.specSize[0] + 2)
+#             self.minIdx = np.argmax(mel_frequencies >= self.minFreq)
+#             self.data = self.data[self.minIdx:, :]
+#             self.specSize = self.data.shape
+        
+#         if self.shuffle:
+#             np.random.shuffle(self.indexes)
+
+#     def __len__(self):
+#         return int(np.ceil(self.num_samples / self.batch_size))
+
+#     def __getitem__(self, index):
+#         start_index = index * self.batch_size
+#         end_index = min((index + 1) * self.batch_size, self.num_samples)
+        
+#         batch_data = []
+#         batch_labels = []
+#         batch_vars = {var: [] for var in self.return_vars}
+
+#         for i in range(start_index, end_index):
+#             key = self.data_keys[self.indexes[i]]
+#             spec = self.hf[self.trainTest][key]['spectrogram'][self.minIdx:, :]
+#             label = self.hf[self.trainTest][key]['label'][()]
+#             batch_data.append(spec)
+#             batch_labels.append(label)
+
+#             for var in self.return_vars:
+#                 if var in self.hf[self.trainTest][key]:
+#                     value = self.hf[self.trainTest][key][var][()]
+#                     batch_vars[var].append(value)
+#                 else:
+#                     raise KeyError(f"Variable '{var}' not found in HDF5 file for key '{key}'")
+
+#         if self.return_data_labels:
+#             return_list = [batch_data, batch_labels]
+#             for var in self.return_vars:
+#                 return_list.append(np.array(batch_vars[var]))
+#             return tuple(return_list)
+#         else:
+#             batch_labels = keras.utils.to_categorical(np.array(batch_labels), num_classes=self.n_classes)
+#             return_list = [np.array(batch_data), batch_labels]
+#             for var in self.return_vars:
+#                 return_list.append(np.array(batch_vars[var]))
+#             return tuple(return_list)
+
+#     def on_epoch_end(self):
+#         if self.shuffle:
+#             np.random.shuffle(self.indexes)
+#             print('Epoch end: all shuffled!')
 
 
 import tensorflow as tf
@@ -400,11 +490,12 @@ def check_spectrogram_dimensions(hdf5_file):
 #########################################################################
 
 
-def create_model_with_resnet(input_shape, num_classes):
+def create_model_with_resnet(input_shape, num_classes, actName = 'relu'):
     input_layer = Input(shape=input_shape)
-    x = Conv2D(32, kernel_size=(3, 3), activation='relu')(input_layer)
+    x = Conv2D(32, kernel_size=(3, 3), activation=actName)(input_layer)
+    #x = BatchNormalization()(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
+    x = Conv2D(64, kernel_size=(3, 3), activation=actName)(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
     
     # Add three identity blocks
@@ -413,51 +504,117 @@ def create_model_with_resnet(input_shape, num_classes):
     x = identity_block(x, 64)
     
     x = Flatten()(x)
-    x = Dense(128, activation='relu')(x)
+    x = Dense(128, activation=actName)(x)
     output_layer = Dense(num_classes, activation='softmax')(x)
     model = Model(inputs=input_layer, outputs=output_layer)
     return model
 
+def create_wider_model(input_shape, num_classes, actName='relu'):
+    input_layer = Input(shape=input_shape)
+    
+    x = Conv2D(64, kernel_size=(3, 3), activation=actName)(input_layer)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    x = Conv2D(128, kernel_size=(3, 3), activation=actName)(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    # Same identity blocks
+    x = identity_block(x, 128)
+    x = identity_block(x, 128)
+    x = identity_block(x, 128)
+    
+    x = Flatten()(x)
+    x = Dense(256, activation=actName)(x)
+    
+    output_layer = Dense(num_classes, activation='softmax')(x)
+    
+    model = Model(inputs=input_layer, outputs=output_layer)
+    return model
+
+
 def identity_block(input_tensor, filters):
     x = Conv2D(filters, kernel_size=(3, 3), activation='relu', padding='same')(input_tensor)
+    #x = BatchNormalization()(x)
     x = Conv2D(filters, kernel_size=(3, 3), activation='relu', padding='same')(x)
     x = Add()([x, input_tensor])  # Add the input tensor to the output of the second convolution
     x = Activation('relu')(x)
     return x
 
 
-
-def create_det_class_model_with_resnet(input_shape, num_classes_ecotype):
+def create_wider_model2(input_shape, num_classes, actName='relu'):
     input_layer = Input(shape=input_shape)
-    x = Conv2D(32, kernel_size=(3, 3), activation='relu')(input_layer)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
-    x = Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
+    
+    x = Conv2D(64, kernel_size=(3, 3), activation=None, padding='same')(input_layer)
+    x = BatchNormalization()(x)
+    x = Activation(actName)(x)
     x = MaxPooling2D(pool_size=(2, 2))(x)
     
-    # Add three identity blocks
-    x = identity_block(x, 64)
-    x = identity_block(x, 64)
-    x = identity_block(x, 64)
+    x = Conv2D(64, kernel_size=(3, 3), activation=None, padding='same')(x)
+    x = BatchNormalization()(x)
+    x = Activation(actName)(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+    # Same identity blocks
+    x = identity_block2(x, 64)
+    x = identity_block2(x, 64)
+    x = identity_block2(x, 64)
     
     x = Flatten()(x)
-    x = Dense(128, activation='relu')(x)
+    x = Dense(128, activation=actName)(x)
     
-    # Output branch for killer whale detection
-    output_killer_whale = Dense(1, activation='sigmoid', name='killer_whale')(x)
+    output_layer = Dense(num_classes, activation='softmax')(x)
     
-    # Output branch for ecotype classification
-    output_ecotype = Dense(num_classes_ecotype, activation='softmax', name='ecotype')(x)
-    
-    model = Model(inputs=input_layer, outputs=[output_killer_whale, output_ecotype])
-    
+    model = Model(inputs=input_layer, outputs=output_layer)
     return model
 
+def identity_block2(input_tensor, filters):
+    x = Conv2D(filters, kernel_size=(3, 3), activation='relu', padding='same')(input_tensor)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    
+    x = Conv2D(filters, kernel_size=(3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization()(x)
+    
+    # Shortcut connection
+    shortcut = Conv2D(filters, kernel_size=(1, 1), activation='relu', padding='same')(input_tensor)
+    shortcut = BatchNormalization()(shortcut)
+    
+    x = Add()([x, shortcut])
+    x = Activation('relu')(x)
+    
+    return x
 
-def compile_det_class_model(model):
-    model.compile(optimizer='adam',
-                  loss={'killer_whale': 'binary_crossentropy', 'ecotype': 'categorical_crossentropy'},
-                  metrics={'killer_whale': 'accuracy', 'ecotype': 'accuracy'})
-    return model
+# def create_det_class_model_with_resnet(input_shape, num_classes_ecotype):
+#     input_layer = Input(shape=input_shape)
+#     x = Conv2D(32, kernel_size=(3, 3), activation='relu')(input_layer)
+#     x = MaxPooling2D(pool_size=(2, 2))(x)
+#     x = Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
+#     x = MaxPooling2D(pool_size=(2, 2))(x)
+    
+#     # Add three identity blocks
+#     x = identity_block(x, 64)
+#     x = identity_block(x, 64)
+#     x = identity_block(x, 64)
+    
+#     x = Flatten()(x)
+#     x = Dense(128, activation='relu')(x)
+    
+#     # Output branch for killer whale detection
+#     output_killer_whale = Dense(1, activation='sigmoid', name='killer_whale')(x)
+    
+#     # Output branch for ecotype classification
+#     output_ecotype = Dense(num_classes_ecotype, activation='softmax', name='ecotype')(x)
+    
+#     model = Model(inputs=input_layer, outputs=[output_killer_whale, output_ecotype])
+    
+#     return model
+
+
+# def compile_det_class_model(model):
+#     model.compile(optimizer='adam',
+#                   loss={'killer_whale': 'binary_crossentropy', 'ecotype': 'categorical_crossentropy'},
+#                   metrics={'killer_whale': 'accuracy', 'ecotype': 'accuracy'})
+#     return model
 
 
 
@@ -475,6 +632,8 @@ def compile_model_customWeights(model, class_weights, priorities):
                   metrics=['accuracy'])
     return model
 
+
+from sklearn.metrics import confusion_matrix
 def batch_conf_matrix(loaded_model, val_batch_loader):
     # Initialize lists to accumulate predictions and true labels
     y_pred_accum = []
@@ -512,11 +671,55 @@ def batch_conf_matrix(loaded_model, val_batch_loader):
     
     return(conf_matrix)
     print("Confusion Matrix:")
+    
+# So this is well and good but I'd like to create be able to look at the timestamps
+# from the predictions so we can do something lie softmax
+
+
+def batch_conf_matrix(loaded_model, val_batch_loader, returnVars = list()):
+    # Initialize lists to accumulate predictions and true labels
+    y_pred_accum = []
+    y_true_accum = []
+    
+    # Get the total number of batches
+    total_batches = len(val_batch_loader)
+    
+    # Iterate over test data generator batches
+    for i in range(0, len(val_batch_loader)):
+        batch_data = val_batch_loader.__getitem__(i)
+        
+        # Predict on the current batch
+        batch_pred = loaded_model.predict(batch_data[0])
+        
+        # Convert predictions to class labels
+        batch_pred_labels = np.argmax(batch_pred, axis=1)
+        
+        # Convert true labels to class labels
+        batch_true_labels = np.argmax(batch_data[1], axis=1)
+        
+        # Accumulate predictions and true labels
+        y_pred_accum.extend(batch_pred_labels)
+        y_true_accum.extend(batch_true_labels)
+        
+        # Print progress
+        print(f'Batch {i+1}/{total_batches} processed')
+    
+    # Convert accumulated lists to arrays
+    y_pred_accum = np.array(y_pred_accum)
+    y_true_accum = np.array(y_true_accum)
+    
+    # Compute confusion matrix
+    conf_matrix = confusion_matrix(y_true_accum, y_pred_accum)
+    
+    return(conf_matrix)
+    print("Confusion Matrix:")
+    
+
 
 ###################################################################
 # Model evaluation
 ###################################################################
-from sklearn.metrics import confusion_matrix
+
 
 # def confuseionMat(model, val_batch_loader):
 #     # Initialize lists to accumulate predictions and true labels
